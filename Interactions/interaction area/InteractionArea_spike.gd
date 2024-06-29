2026c63c58142f3894c7e12d72886ac649faec3e
mrolain <rolainmilan@gmail.com>
2024-06-29 22:22:17 +0200

    spike button synchronized

--- Interactions/interaction area/InteractionArea_spike.gd
+++ Interactions/interaction area/InteractionArea_spike.gd
@@ -1,16 +1,16 @@
-extends Area2D
+extends Node2D
 class_name InteractionArea_spike
 
-
-@onready var label_spike = $Label_spike
-@onready var spike = $"../../../Spike"
+@onready var label_spike = $spike_button/InteractionArea_spike/Label_spike
+@export var spike : Area2D
 var player_in_spike_area = false
+@onready var interaction_area_spike = $spike_button/InteractionArea_spike
 
 
 func _ready():
-	#label_spike.hide()
-	connect("body_entered", _on_body_entered)
-	connect("body_exited", _on_body_exited)
+	label_spike.hide()
+	interaction_area_spike.connect("body_entered", _on_body_entered)
+	interaction_area_spike.connect("body_exited", _on_body_exited)
 	#label_spike.hide()
 
 
