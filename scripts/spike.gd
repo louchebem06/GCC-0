61aa44245cf106b96074f9680fb2e16660180a28
mrolain <rolainmilan@gmail.com>
2024-06-30 10:39:59 +0200

    functional trap

--- scripts/spike.gd
+++ scripts/spike.gd
@@ -1,21 +1,21 @@
 extends Area2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var spike_collision = $spike_collision
+@onready var collision = $collision
 
 
 @export var is_on : bool = false
 
 
 func _ready():
-	spike_collision.disabled = true
+	collision.disabled = true
 
  
-func activate_spikes():
+func activate_trap():
 	is_on = true
 	animated_sprite_2d.play("on")
 	await get_tree().create_timer(0.7).timeout
-	spike_collision.disabled = false
+	collision.disabled = false
 	await get_tree().create_timer(0.9).timeout
-	spike_collision.disabled = true
+	collision.disabled = true
 	is_on = false
